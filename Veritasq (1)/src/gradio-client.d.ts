1770f57e62173280454f7fa2e1eef544caf89176
uniqgenie <incredimee@gmail.com>
2025-11-13 23:11:53 +0100

    Delete Veritasq (1) directory

--- Veritasq (1)/src/gradio-client.d.ts
+++
@@ -1,1 +0,0 @@
-declare module "@gradio/client";
--- Veritasq (1)/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_HF_SPACE_ID?: string;
-  readonly VITE_HF_TOKEN?: string;
-}
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
